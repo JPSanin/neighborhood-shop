2b6b8385aa554d6b32b5a7d945fbcedd15eeacaa
Juan Sanin <juanpablosaninb@gmail.com>
2020-08-21 15:11:08 -0500

    Final Edits

--- test/model/RegisterTest.java
+++ test/model/RegisterTest.java
@@ -10,9 +10,14 @@ class RegisterTest {
 
 	public void setupStage1() {
 		s= new Shop();	
+		
+	}
+	
+	@Test
+	void testRegister1() {
+		setupStage1();
 		int date= LocalDate.now().getDayOfMonth();
 		int lastNumDate= date%10;
-		
 		try {
 			if (lastNumDate%2==0) {
 				s.register(2, 79311977);
@@ -22,22 +27,33 @@ class RegisterTest {
 		}catch(CannotRegisterException cre){
 			fail("Cannot register exception is not expected here");
 		}
-	}
-
-	public void setupStage2() {
-		s= new Shop();	
-		
-		try {
-				s.register(1, 79311977);
-				fail("Cannot register exception is expected");
-		}catch(CannotRegisterException cre){
-			
-			
+		if (lastNumDate%2==0) {
+			assertEquals(2,s.getRegisteredPeople().get(0).getDocType(),"The document type is wrong");
+			assertEquals(79311977,s.getRegisteredPeople().get(0).getDocNum(),"The document number is wrong");
+		}else {
+			assertEquals(2,s.getRegisteredPeople().get(0).getDocType(),"The document type is wrong");
+			assertEquals(79311987,s.getRegisteredPeople().get(0).getDocNum(),"The document number is wrong");
 		}
+		assertEquals(1,s.getTriedToRegister(),"The number of people that have tried to register is wrong");
 	}
 	
-	public void setupStage3() {
-		s= new Shop();	
+	@Test
+	void testRegister2() {
+		setupStage1();
+		try {
+			s.register(1, 79311977);
+			fail("Cannot register exception is expected");
+	}catch(CannotRegisterException cre){
+		
+		
+	}
+		assertEquals(0,s.getRegisteredPeople().size(), "The list is not empty");
+		assertEquals(1,s.getTriedToRegister(),"The number of people that have tried to register is wrong");
+	}
+		
+	@Test
+	void testRegister3() {
+		setupStage1();
 		int date= LocalDate.now().getDayOfMonth();
 		int lastNumDate= date%10;
 		
@@ -51,29 +67,6 @@ class RegisterTest {
 		}catch(CannotRegisterException cre){
 			
 		}
-	}
-	
-
-	
-	@Test
-	void testRegister() {
-		setupStage1();
-		int date= LocalDate.now().getDayOfMonth();
-		int lastNumDate= date%10;
-		if (lastNumDate%2==0) {
-			assertEquals(2,s.getRegisteredPeople().get(0).getDocType(),"The document type is wrong");
-			assertEquals(79311977,s.getRegisteredPeople().get(0).getDocNum(),"The document number is wrong");
-		}else {
-			assertEquals(2,s.getRegisteredPeople().get(0).getDocType(),"The document type is wrong");
-			assertEquals(79311987,s.getRegisteredPeople().get(0).getDocNum(),"The document number is wrong");
-		}
-		assertEquals(1,s.getTriedToRegister(),"The number of people that have tried to register is wrong");
-		
-		setupStage2();
-		assertEquals(0,s.getRegisteredPeople().size(), "The list is not empty");
-		assertEquals(1,s.getTriedToRegister(),"The number of people that have tried to register is wrong");
-		
-		setupStage3();
 		assertEquals(0,s.getRegisteredPeople().size(), "The list is not empty");
 		assertEquals(1,s.getTriedToRegister(),"The number of people that have tried to register is wrong");
 	}
