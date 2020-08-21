452124d2d21ffd060f09727d4024278108c6e3ca
Juan Sanin <juanpablosaninb@gmail.com>
2020-08-20 19:47:52 -0500

    Class development

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,4 +2,74 @@ package model;
 
 public class Person {
 
+	//Constants representing the types of documents
+	private final static int TI=1;
+	private final static int CC=2;
+	private final static int PP=3;
+	private final static int FC=4;
+	private int docType;
+	private int docNum;
+
+	public Person(int docType, int docNum) {
+		switch(docType) {
+		case TI:
+			this.docType=TI;
+			break;
+
+		case CC:
+			this.docType=CC;
+			break;
+
+		case PP:
+			this.docType=PP;
+			break;
+
+		case FC:
+			this.docType=FC;
+			break;
+		}
+
+		this.docNum=docNum;
+	}
+
+	public String printInfo() {
+		String info="";
+		info+="Document Type:";
+		switch(docType) {
+		case TI:
+			info+=" Identity Card";
+			break;
+
+		case CC:
+			info+=" Citizen Card";
+			break;
+
+		case PP:
+			info+=" Passport";
+			break;
+
+		case FC:
+			info+=" Foreigner Citizen Card";
+			break;
+		}
+		info+="\n Document Number: "+docNum;
+		
+		
+		return info;
+	}
+
+
+	public int getDocType() {
+		return docType;
+	}
+	public void setDocType(int docType) {
+		this.docType = docType;
+	}
+	public int getDocNum() {
+		return docNum;
+	}
+	public void setDocNum(int docNum) {
+		this.docNum = docNum;
+	}
+
 }
--- src/model/Shop.java
+++ src/model/Shop.java
@@ -1,5 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+import exceptions.CannotRegisterException;
+import java.time.LocalDate;
 public class Shop {
+	private final static int TI=1;
+	private ArrayList<Person> registeredPeople;
+
+	public Shop() {
+		registeredPeople= new ArrayList<Person>();
+	}
+	
+	public void register(int docType, int docNum) throws CannotRegisterException {
+		int date= LocalDate.now().getDayOfMonth();
+		int lastNumDate= date%10;
+		int secondToLastNum= ((docNum/10)%10);
+		if(docType==TI) {
+			throw new CannotRegisterException(docType, docNum);
+		}else if((secondToLastNum%2==0 && lastNumDate%2 ==0) || (secondToLastNum%2!=0 && lastNumDate%2 !=0)) {
+			throw new CannotRegisterException(docType, docNum);
+		}
+		
+	Person p= new Person(docType,docNum);
+	registeredPeople.add(p);
+		
+		
+	}
+
+	public ArrayList<Person> getRegisteredPeople() {
+		return registeredPeople;
+	}
 
 }
